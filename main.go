78fcee812e50b5eb5c73771c1283409944d8dcaa
ryane <ryanesc@gmail.com>
2016-03-09 18:34:36 -0500

    treat sync errors as fatal
    a failed sync will cause mantl-api to fail. it will re-sync, if needed,
    when restarted.

--- main.go
+++ main.go
@@ -243,7 +243,9 @@ func syncRepo(inst *install.Install, force bool) {
 		sources = defaultSources
 	}
 
-	inst.SyncSources(sources, force)
+	if err := inst.SyncSources(sources, force); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func readConfigFile() {
